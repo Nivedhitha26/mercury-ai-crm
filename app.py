95761e43f27c252b0296a83a82ad6a26479886dd
Nivedhitha26 <162608400+Nivedhitha26@users.noreply.github.com>
2026-07-11 20:42:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import joblib
 app = Flask(__name__)
 
 case_model = joblib.load("case_classifier_logistic.pkl")
+priority_model = joblib.load("priority_text_classifier.pkl")
 
 @app.route("/")
 def home():
-    return "Case Model Loaded Successfully"
+    return "Both Models Loaded Successfully"
 
 if __name__ == "__main__":
     app.run()
