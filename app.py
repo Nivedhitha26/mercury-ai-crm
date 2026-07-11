3ba5489b2aadbb3deac08db052a3739e54cfb164
Nivedhitha26 <162608400+Nivedhitha26@users.noreply.github.com>
2026-07-11 20:37:56 +0530

    Load case model in Flask application

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import joblib
 
 app = Flask(__name__)
 
+case_model = joblib.load("case_classifier_logistic.pkl")
+
 @app.route("/")
 def home():
-    return "Joblib Imported Successfully"
+    return "Case Model Loaded Successfully"
 
 if __name__ == "__main__":
     app.run()
